8a2b22db029a3a2aadb1bf57d7375b2223f1ce81
Chris Deaner <deaner@monsterbit.com>
2014-06-16 14:57:38 -0400

    stubbing out some travis and grunt build stuff

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    //pkg: grunt.file.readJSON('package.json'),
+    copy: {
+	  assets: {
+	    src: [
+	    	'./index.html',
+	    	'./images/**',
+	    	'./dep/**',
+	    	'./app/**'
+	    ],
+	    dest: './build/release/www/',
+	  }
+	},
+	clean: {
+	  release: [ './build/release/'],
+	  releaseFiles: [ './release/*.wav','./release/*.scss','./release/*.DS_STORE', './release/www/assets/images/deviceAssets/', './release/www/build/grid.js']
+	},
+	cssmin: {
+		minify: {
+		    src: './release/www/style/main.css',
+		    dest: './release/www/style/main.min.css'
+		}
+	}
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  //grunt.registerTask('default', ['clean:release','copy:assets', 'cssmin','clean:releaseFiles']);
+  grunt.registerTask('default', ['clean:release','copy:assets']);
+  
+};
---
+++ build/release/www/app/controller/Mediator.js
@@ -0,0 +1,60 @@
+define(function(){
+	
+	/**
+	 * MicroEvent - to make any js object an event emitter (server or browser)
+	 * 
+	 * - pure javascript - server compatible, browser compatible
+	 * - dont rely on the browser doms
+	 * - super simple - you get it immediatly, no mistery, no magic involved
+	 *
+	 * - create a MicroEventDebug with goodies to debug
+	 *   - make it safer to use
+	*/
+
+	var MicroEvent	= function(){};
+
+	MicroEvent.prototype	= {
+		route	: function(event, fct){
+			this._events = this._events || {};
+			this._events[event] = this._events[event]	|| [];
+			this._events[event].push(fct);
+		},
+		unroute	: function(event, fct){
+			this._events = this._events || {};
+			if( event in this._events === false  )	return;
+			this._events[event].splice(this._events[event].indexOf(fct), 1);
+		},
+		send	: function(event /* , args... */){
+			this._events = this._events || {};
+			if( event in this._events === false  )	return;
+			for(var i = 0; i < this._events[event].length; i++){
+				this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
+			}
+		}
+	};
+
+	var Mediator = new MicroEvent();
+
+	//////////////////////////////////////////////////////////////////////////
+	//	UPDATE LOOP
+	///////////////////////////////////////////////////////////////////////////
+	
+	function updateLoop(){
+		requestAnimFrame(updateLoop);
+		Mediator.send("update");
+	}
+
+	updateLoop();
+
+	return Mediator;
+});
+
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+		  window.webkitRequestAnimationFrame ||
+		  window.mozRequestAnimationFrame    ||
+		  function( callback ){
+			window.setTimeout(callback, 1000 / 60);
+		  };
+})();
