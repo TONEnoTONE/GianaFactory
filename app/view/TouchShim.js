483461d30f644cd19a6ef4cd1270156894f216da
Yotam Mann <yotammann@gmail.com>
2014-06-25 14:12:42 -0400

    audio files

---
+++ app/view/TouchShim.js
@@ -0,0 +1,14 @@
+define(["jquery"], function($){
+
+	function triggerTouch(e){
+		var touches = e.originalEvent.touches;
+		for (var i = 0; i < touches.length; i++){
+			var touch = touches.item(i);
+			var element = document.elementFromPoint(touch.clientX, touch.clientY);
+			$(element).trigger("touch");
+		}
+	}
+
+	$(document).on("touchmove", triggerTouch);
+	$(document).on("touchstart", triggerTouch);
+});
